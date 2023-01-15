7ff83e4f27de358deb0c8341fd8fddb93eb25a85
VTRMAZ <victormazloum@gmail.com>
2023-01-15 15:53:58 +0100

    mise en place de la libraires flask

---
+++ TESTSITEFLASH/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='myproject',
+    version='1.0',
+    author='John Doe',
+    author_email='johndoe@example.com',
+    description='This is my sample project',
+    install_requires=[
+        'flask',
+        'selenium'
+    ],
+)
